810f347f9e5d4ed62576519457dcffd91e33b9ff
Pravalika Buggareddy <pravallikabuggareddy@gmail.com>
2022-03-12 22:20:05 +0530

    commit initial

---
+++ src/test/java/Calculator_functionsTest.java
@@ -0,0 +1,16 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Calculator_functionsTest {
+    public static Calculator_functions obj;
+
+    @BeforeClass
+    public static void setup(){obj = new Calculator_functions();}
+
+    @Test
+    public void test_add(){
+        assertEquals(10,obj.add(5,5));
+    }
+}
